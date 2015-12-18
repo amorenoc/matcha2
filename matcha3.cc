167295b84731e82a9d3246be30dcc63983cc55a6
Alex Moreno <alex_moreno@tutk.com>
2015-12-18 20:22:38 +0800

    misc.

--- matcha3.cc
+++ matcha3.cc
@@ -10,230 +10,180 @@
 #include <algorithm>
 #include <iterator>
 #include <valarray>
+#include <typeinfo>
 
+#include "prettyprint.hpp"
 
 namespace matcha {
 
-// SFINAE type trait to detect whether T::const_iterator exists.
+  template<typename T>
+  struct is_container : pretty_print::is_container<T> 
+  { };
 
-template<typename T>
-struct has_const_iterator
-{
-private:
-    typedef char                      yes;
-    typedef struct { char array[2]; } no;
+  template<typename T, typename... Rest>
+  struct is_same : std::false_type
+  { };
 
-    template <typename C> static yes test(typename C::const_iterator*);
-    template <typename C> static no  test(...);
-public:
-    static const bool value = sizeof(test<T>(0)) == sizeof(yes);
-    typedef T type;
-};
+  template<typename T, typename First>
+  struct is_same<T, First> : std::is_same<T, First>
+  { };
 
-// SFINAE type trait to detect whether "T::const_iterator T::begin/end() const" exist.
+  template<typename T, typename First, typename... Rest>
+  struct is_same<T, First, Rest...>
+    : std::integral_constant<
+        bool,
+        std::is_same<T, First>::value && is_same<T, Rest...>::value
+      >
+  { };
 
-template <typename T>
-struct has_begin_end_OLD
-{
-    struct Dummy { typedef void const_iterator; };
-    typedef typename std::conditional<has_const_iterator<T>::value, T, Dummy>::type TType;
-    typedef typename TType::const_iterator iter;
-
-    struct Fallback { iter begin() const; iter end() const; };
-    struct Derived : TType, Fallback { };
-
-    template<typename C, C> struct ChT;
-
-    template<typename C> static char (&f(ChT<iter (Fallback::*)() const, &C::begin>*))[1];
-    template<typename C> static char (&f(...))[2];
-    template<typename C> static char (&g(ChT<iter (Fallback::*)() const, &C::end>*))[1];
-    template<typename C> static char (&g(...))[2];
-
-    static bool const beg_value = sizeof(f<Derived>(0)) == 2;
-    static bool const end_value = sizeof(g<Derived>(0)) == 2;
-};
-
-template <typename T>
-struct has_begin_end
-{
-    template<typename C> static char (&f(typename std::enable_if<
-      std::is_same<decltype(static_cast<typename C::const_iterator (C::*)() const>(&C::begin)),
-      typename C::const_iterator(C::*)() const>::value, void>::type*))[1];
-
-    template<typename C> static char (&f(...))[2];
-
-    template<typename C> static char (&g(typename std::enable_if<
-      std::is_same<decltype(static_cast<typename C::const_iterator (C::*)() const>(&C::end)),
-      typename C::const_iterator(C::*)() const>::value, void>::type*))[1];
-
-    template<typename C> static char (&g(...))[2];
-
-    static bool const beg_value = sizeof(f<T>(0)) == 1;
-    static bool const end_value = sizeof(g<T>(0)) == 1;
-};
-
-// Basic is_container template; specialize to derive from std::true_type for all desired container types
-
-template<typename T> struct is_container : public ::std::integral_constant<bool,
-  has_const_iterator<T>::value && has_begin_end<T>::beg_value && has_begin_end<T>::end_value> { };
-
-template<typename T, std::size_t N> struct is_container<T[N]> : public ::std::true_type { };
-
-template<std::size_t N> struct is_container<char[N]> : public ::std::false_type { };
-
-template <typename T> struct is_container< ::std::valarray<T>> : public ::std::true_type { };
-
-template<typename T, typename... Rest>
-struct is_same : std::false_type
-{ };
-
-template<typename T, typename First>
-struct is_same<T, First> : std::is_same<T, First>
-{ };
-
-template<typename T, typename First, typename... Rest>
-struct is_same<T, First, Rest...>
-  : std::integral_constant<
-      bool,
-      std::is_same<T, First>::value && is_same<T, Rest...>::value
-    >
-{ };
-
-template<typename T>
-struct IsEqual
-{
-  bool matches(const T & actual, const T & expected) {
-    std::cout << "equal to " << expected << '\n';
-    return false;
-  }
-
-};
-
-template<class...>
-struct IsContaining;
-
-template<class T>
-struct IsContaining<T>
-{
-  template<class C>
-  bool matches(const C & actual, const T & expected)
+  template<template <class...> class Predicate, class ... Ts>
+  class Matcher
   {
-    static_assert(is_container<C>::value, "expects a Container");
+  public:
+    Matcher(Ts&& ... args) : pred(), args(std::forward<Ts>(args)...) { }
+    template<class T> bool matches(const T &);
 
-    std::for_each(std::begin(actual), std::end(actual), [](auto &n) { 
-      std::cout << "val is " << n << '\n';
-    });
-    return false;
-  }
-};
+  private:
+    template <class T, std::size_t... Is>
+    bool matches_impl(const T & actual, std::index_sequence<Is...>);
 
-template<class Key, class T>
-struct IsContaining<Key,T>
-{
-  template<class C>
-  bool matches(const C & actual, const Key key, const T & value)
+  private:
+    Predicate<std::decay_t<Ts>...> pred;
+    std::tuple<Ts...> args;
+  };
+
+  template<template <class...> class Predicate, class ... Ts>
+  template <class T, std::size_t... Is>
+  bool Matcher<Predicate,Ts...>::matches_impl(const T & actual, std::index_sequence<Is...>)
   {
-    return false;
+    return pred.matches(actual, std::get<Is>(args)...);
   }
 
-};
-
-template<typename T>
-struct EndsWith;
-
-template<>
-struct EndsWith<const std::string &>
-{
-  bool matches(std::string actual, std::string expected) {
-    std::cout << "actual is " << actual << "expected " << expected << '\n';
-    return false;
+  template<template <class...> class Predicate, class ... Ts>
+  template<class T>
+  bool Matcher<Predicate,Ts...>::matches(const T & actual)
+  {
+    return matches_impl(actual, std::index_sequence_for<Ts...>{});
   }
-};
 
-template<class ... Ts>
-struct AnyOf
-{
-  static_assert(is_same<Ts...>::value, 
-    "IsNot matcher requires a Matcher parameter");
+  /*
+   * factory functions
+   */
+  template<template <class...> class Predicate, class ... T>
+  auto make_matcher(T && ... val) 
+  {
+    return Matcher<Predicate, T...>(std::forward<T>(val)...);
+  }
+
+
+  template<typename T, class = void>
+  struct IsEqual
+  {
+    bool matches(const T & actual, const T & expected)
+    {
+      std::cout << "not container" << std::endl;
+      return actual == expected;
+    }
+  };
+
+  template <typename T>
+  struct IsEqual<T, std::enable_if_t<is_container<T>::value>> 
+  {
+    template<typename U>
+    bool matches(const U & actual, const T & expected)
+    {
+      using std::begin;
+      using std::end;
+
+      std::cout << "container" << std::endl;
+      return std::equal(begin(actual),   end(actual),
+                        begin(expected), end(expected));
+    }
+  };
+
+  template<class...>
+  struct IsContaining;
 
   template<class T>
-  bool matches(const T & actual, const Ts & ... args) {
-    for (auto&& x : { args... }) {
-      std::cout << "anyof is " << x << '\n';
-//            if (x.matches(actual))
-//                return true;
-    }
-    return false;
-  }
-};
-
-template<class T, class ... Ts>
-struct OneOf
-{
-  bool matches(const T & actual, const T & first, const Ts & ... rest) 
+  struct IsContaining<T>
   {
-    for (auto&& x : { first, rest... }) {
-      std::cout << "x is " << x << '\n';
+    template<class C>
+    bool matches(const C & actual, const T & expected)
+    {
+      static_assert(is_container<C>::value, "expects a Container");
+
+      std::for_each(std::begin(actual), std::end(actual), [](auto &n) { 
+        std::cout << "val is " << n << '\n';
+      });
+      return false;
     }
-    return false;
-  }
-};
+  };
 
-template<typename...>
-struct IsNull;
+  template<class Key, class T>
+  struct IsContaining<Key,T>
+  {
+    template<class C>
+    bool matches(const C & actual, const Key key, const T & value)
+    {
+      return false;
+    }
 
-template<>
-struct IsNull<>
-{
+  };
+
   template<typename T>
-  bool matches(const T & actual)
+  struct EndsWith;
+
+  template<>
+  struct EndsWith<std::string>
   {
-    std::cout << "actual is null? " << actual << '\n';
-    return false;
-  }
-};
+    bool matches(std::string actual, std::string expected) {
+      std::cout << "actual is " << actual << "expected " << expected << '\n';
+      return false;
+    }
+  };
 
-template<template <class...> class Predicate, class ... Ts>
-class Matcher : public Predicate<Ts...>
-{
-public:
-  Matcher(Ts&& ... args) : args(std::forward<Ts>(args)...) { }
+  template<class ... Ts>
+  struct AnyOf
+  {
+    static_assert(is_same<Ts...>::value, 
+      "IsNot matcher requires a Matcher parameter");
 
-  template<class T, class indices = std::index_sequence_for<Ts...>>
-  bool matches(const T & actual);
+    template<class T>
+    bool matches(const T & actual, const Ts & ... args) {
+      for (auto&& x : { args... }) {
+        std::cout << "anyof is " << x << '\n';
+  //            if (x.matches(actual))
+  //                return true;
+      }
+      return false;
+    }
+  };
 
-private:
-  template <class T, std::size_t... I>
-  bool dispatch(const T & actual, std::index_sequence<I...>);
+  template<class T, class ... Ts>
+  struct OneOf
+  {
+    bool matches(const T & actual, const T & first, const Ts & ... rest) 
+    {
+      for (auto&& x : { first, rest... }) {
+        std::cout << "x is " << x << '\n';
+      }
+      return false;
+    }
+  };
 
-private:
-  std::tuple<Ts...> args;
-};
+  template<typename...>
+  struct IsNull;
 
-template<template <class...> class Predicate, class ... Ts>
-template <class T, std::size_t... I>
-bool Matcher<Predicate,Ts...>::dispatch(const T & actual, std::index_sequence<I...>)
-{
-  return Predicate<Ts...>::matches(actual, std::get<I>(args)...);
-}
-
-template<template <class...> class Predicate, class ... Ts>
-template<class T, class indices>
-bool Matcher<Predicate,Ts...>::matches(const T & actual)
-{
-  return dispatch(actual, indices());
-}
-
-/*
- * factory functions
- */
-template<template <class...> class Predicate, class ... T>
-auto make_matcher(T && ... val) 
-{
-  std::cout << "bar" << '\n';
-  return Matcher<Predicate, T...>
-    (std::forward<T>(val)...);
-}
+  template<>
+  struct IsNull<>
+  {
+    template<typename T>
+    bool matches(const T & actual)
+    {
+      std::cout << "actual is null? " << actual << '\n';
+      return false;
+    }
+  };
 
 }; // end matcha
 
@@ -297,13 +247,17 @@ int main()
 
   std::array<int,3> foo = {5,2,3};
   expect(foo, contain(3));
+  int xx[] = {1,2,3};
+  expect(xx, equals(foo));
 
-  const std::map<std::string, int> bar {
+  std::map<std::string, int> bar {
     {"this",  100},
     {"can",   100},
     {"be",    100},
     {"const", 100},
   };
+
+  expect(bar, equals(bar));
 
   expect(bar, contain("string", 100));
   //expect(std::begin(foo), std::end(foo), contains(3));
